d2757d245e4aec70478ef7db14f662dd16afe0cd
Taher Huzefa <taherhuzefa@Tahers-MacBook-Pro.local>
2018-11-26 19:56:32 -0600

    The arrow keys are working

--- GUI.java
+++ GUI.java
@@ -7,15 +7,26 @@ public class GUI extends JFrame
 
 
    private int[][] puzzle =
-      {{5, 3, 4, 6, 7, 8, 9, 1, 2},
-       {6, 7, 2, -1, 9, 5, 3, 4, 8},
-       {-1, 9, 8, 3, 4, 2, -1, 6, 7},
-       {8, 5, -1, 7, 6, 1, 4, 2, 3},
-       {4, 2, 6, -1, 5, 3, 7, 9, 1},
-       {7, 1, 3, 9, 2, 4, 8, 5, 6},
-       {9, 6, 1, 5, 3, 7, 2, 8, 4},
-       {2, 8, 7, 4, 1, -1, 6, 3, 5},
-       {3, 4, 5, 2, 8, 6, -1, 7, 9}};
+      {{9, 0, 0, 1, 0, 0, 0, 0, 5},
+       {0, 0, 5, 0, 9, 0, 2, 0, 1},
+       {8, 0, 0, 0, 4, 0, 0, 0, 0},
+       {0, 0, 0, 0, 8, 0, 0, 0, 0},
+       {0, 0, 0, 7, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 2, 6, 0, 0, 9},
+       {2, 0, 0, 3, 0, 0, 0, 0, 6},
+       {0, 0, 0, 2, 0, 0, 9, 0, 0},
+       {0, 0, 1, 9, 0, 4, 5, 7, 0}};
+   private int [][] solution =
+   {{ 9, 3, 4, 1, 7, 2, 6, 8, 5},
+ {7, 6, 5, 8, 9, 3, 2, 4, 1},
+ {8, 1, 2, 6, 4, 5, 3, 9, 7},
+ {4, 2, 9, 5, 8, 1, 7, 6, 3},
+ {6, 5, 8, 7, 3, 9, 1, 2, 4},
+ {1, 7, 3, 4, 2, 6, 8, 5, 9},
+ {2, 9, 7, 3, 5, 8, 4, 1, 6} ,
+ {5, 4, 6, 2, 1, 7, 9, 3, 8},
+ {3, 8, 1, 9, 6, 4, 5, 7, 2}};
+	private boolean[][] empty =new boolean[9][9];
 
       private JTextField[][] inputs = new JTextField[9][9];
       private JPanel[][] borders = new JPanel[3][3];
@@ -40,6 +51,9 @@ public GUI()
 	reset.setName("taher");
 	resetbutton.add(reset);
 	
+	empty = mask(empty);
+
+
 
 //	JPanel pane = new JPanel();
 //	pane.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -64,13 +78,14 @@ for(int i=0;i<3;i++)
 			inputs[i][j] = new JTextField();
 			inputs[i][j].setBorder(BorderFactory.createLineBorder(new Color(214,212,247)));
 			inputs[i][j].setBackground(new Color(240,240,240));
-			inputs[i][j].setHorizontalAlignment(JTextField.CENTER);
+			inputs[i][j].setHorizontalAlignment(JTextField.CENTER);			
+			inputs[i][j].setForeground(Color.GREEN);
 			inputs[i][j].setFont(new Font("Monospaced",Font.BOLD,21));
 			inputs[i][j].setFocusable(true);
 			inputs[i][j].addActionListener(new Listener(inputs[i][j]));
 			inputs[i][j].addKeyListener(new keylistener());
 			inputs[i][j].setName(i+""+j+"");
-			if(puzzle[i][j]<0)
+			if(puzzle[i][j]<=0)
 			{
 				inputs[i][j].setText("");
 				inputs[i][j].setEditable(true);			
@@ -125,7 +140,19 @@ for(int i=0;i<9;i++)
 //add(pane);
 }
 
+private boolean[][] mask(boolean[][] arr)
+{
+	for(int i=0;i<9;i++)
+		for(int j=0;j<9;j++)
+		{
+			if (puzzle[i][j]<=0)
+				arr[i][j]=false;
+			else
+				arr[i][j]=true;
+		}
 
+		return arr;
+}
 
 
 private class Listener implements ActionListener
@@ -157,7 +184,7 @@ private class keylistener implements KeyListener
 
 	   public void keyTyped(KeyEvent e) {
 //    	inputs[2][6].requestFocusInWindow();
-
+	   	reset.setText("");
         //System.out.println(e);
         System.out.println(e);
           if(e.getKeyChar() == 'a')
@@ -169,20 +196,117 @@ private class keylistener implements KeyListener
 
     /** Handle the key-pressed event from the text field. */
     public void keyPressed(KeyEvent e) {
-    //   inputs[2][6].requestFocusInWindow();
-
+    
+    	int i=0,j=0;
+    	String result;
         String index=((JTextField) e.getSource()).getName();
-        System.out.println(index.charAt(0)+"_"+index.charAt(1));
-        System.out.println(((JTextField) e.getSource()).getName());
-        int i = (int) index.charAt(0);
-        int j = (int) index.charAt(1);
+        i = (int) index.charAt(0);
+        j = (int) index.charAt(1);
+        if(e.getKeyChar()=='c' && index.equals("taher"))
+        {
+        	
+        //System.out.println("This is the start of how it ever ends"+ inputs[i][j].getText());
+        for( i=0;i<9;i++)
+        	for( j=0;j<9;j++)
+        	{
+        	
+
+        			result=inputs[i][j].getText();
+if(!result.equals("")){
+        		if( Integer.parseInt(result) == solution[i][j] )
+        		{
+        			inputs[i][j].setEditable(false);
+        			inputs[i][j].setForeground(Color.GREEN);
+        			empty[i][j]=true;
+        		}
+        		else{
+        			inputs[i][j].setForeground(Color.BLUE);
+        		}
+
+        	}
+					}	
+
+
+        }
+        
+
+if(!index.equals("taher")){
+    //    System.out.println(i+"_"+j);
+      	int arrow= e.getKeyCode();
+      	        //System.out.println(e.getName());
+      System.out.println(Integer.parseInt(index.substring(1,2)));
+      i = Integer.parseInt(index.substring(0,1));
+      j=Integer.parseInt(index.substring(1,2));
+          System.out.println(i+"_"+j);
+      	switch(arrow)
+      	{
+      		case 38:
+      			for(int k=i-1;k>=0;k--)
+      			{	System.out.println("hahah"+"_"+inputs[k][j].getText());
+      				System.out.println(k+"____"+j);
+      				if(inputs[k][j].getText().equals(""))
+      					{	System.out.println("This is the start of how it ever ends");
+      						inputs[k][j].requestFocusInWindow();
+      						break;
+      					}
+      			}
+      			break;
+
+			case 40:
+      			for(int k=i+1;k<=8;k++)
+      			{	System.out.println("hahah"+"_"+inputs[k][j].getText());
+      				System.out.println(k+"____"+j);
+      				if(inputs[k][j].getText().equals(""))
+      					{	System.out.println("This is the start of how it ever ends");
+      						inputs[k][j].requestFocusInWindow();
+      						break;
+      					}
+      			}
+      			break;      	
+
+
+      		case 39:
+      				for(int k=j+1;k<=8;k++)
+      			{	System.out.println("hahah"+"_"+inputs[i][k].getText());
+      				System.out.println(k+"____"+j);
+      				if(inputs[i][k].getText().equals(""))
+      					{	System.out.println("This is the start of how it ever ends");
+      						inputs[i][k].requestFocusInWindow();
+      						break;
+      					}
+      			}
+
+
+      			break;
+
+      	
+      		case 37:
+      				for(int k=j-1;k>=0;k--)
+      			{	System.out.println("hahah"+"_"+inputs[i][k].getText());
+      				System.out.println(k+"____"+j);
+      				if(inputs[i][k].getText().equals(""))
+      					{	System.out.println("This is the start of how it ever ends");
+      						inputs[i][k].requestFocusInWindow();
+      						break;
+      					}
+      			}
+
+
+      			break;
+
+
+
+      	}
+
+
+
+        
+        }
       
     }
 
-    /** Handle the key-released event from the text field. */
-    public void keyReleased(KeyEvent e) {
-       // inputs[2][6].requestFocusInWindow();
-    }
+    
+    public void keyReleased(KeyEvent e){}
 
 
 
